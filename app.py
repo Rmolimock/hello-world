1cba4a50586a4ae5922fe315f7b46397e3d5730c
Russell Molimock <38747720+Rmolimock@users.noreply.github.com>
2021-06-22 18:30:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',  methods=['GET'], strict_slashes=False)
 def index():
-  return '<h1>Hello, World!</h1>'
+  return '<h1>Hello,1 World!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, port='8080', host='0.0.0.0')
