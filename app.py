190bc2f6c4679c7d243565cd058e7001fbfbd920
Russell Molimock <38747720+Rmolimock@users.noreply.github.com>
2021-06-23 20:06:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/',  methods=['GET'], strict_slashes=False)
 def index():
-  return jsonify('<h1>Hello, World!</h1>')
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port='8080', host='0.0.0.0')
